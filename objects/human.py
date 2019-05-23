3f5955a003b6d7ec84165ec77f77ae5347416d5a
HolidayMan <lesha.pitenko791@gmail.com>
2019-05-23 23:42:03 +0300

    some small changes

--- objects/human.py
+++ objects/human.py
@@ -4,34 +4,19 @@ import random
 class Human(Creature):
 
     class_name = "human"
-    lifes = 10
-    description = "Human"
-    isAlive = True
+    body_parts = ['right arm', 'left arm', 'right leg', 'left leg', 'head', 'body']
+    _description = "Human"
 
-    def __init__(self, name, age, sex, damage=1, job=None):
+    def __init__(self, name, age, sex, damage=1, lifes=10, job=None):
         super().__init__(name)
         
         self.age = age
         self.sex = sex
-        self.damage = 1
+        self.damage = damage
+        self.max_lifes = lifes
+        self.lifes = lifes
 
         if job != None:
             self.job = job
 
-    @property
-    def description(self):
-    	if self.isAlive:
-    		return "{}, {}, age={}, sex={}, lifes={}".format(self.class_name, self.name, self.age, self.sex, self.lifes)
-    	else:
-    		return "Died"
 
-    def hit(self, obj):
-    	if obj.name in Creature.objects.keys():
-    		obj.get_damage(damage)
-    	else:
-    		raise NameError("This character doesn't exist")
-
-
-    def get_damage(self, damage_given):
-    	self.lifes -= damage_given
-    	print("Lifes left: {}".format(self.lifes))
--- objects/main_character.py
+++ objects/main_character.py
@@ -4,7 +4,6 @@ class MainCharacter(Human):
 
     class_name = "main character"
     _description = "Main character"
-    lifes = 15
 
-    def __init__(self, name, age, sex, damage=2, job=None):
-        super().__init__(name, age, sex, job=None)
+    def __init__(self, name, age, sex, damage=2, lifes=15, job=None):
+        super().__init__(name, age, sex, damage=damage, lifes=lifes, job=None)
